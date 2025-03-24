08de84185777bdf48504f0b5dc68e9d180414c68
vagaojf <vagaojf@gmail.com>
2025-03-24 10:57:26 -0300

    Finalizaçãodo basico do app

--- MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,9 +1,34 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class EditarProduto : ContentPage
 {
 	public EditarProduto()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+            Produto produto_anexado = BindingContext as Produto;
+            Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+            await App.Db.update(p);
+            await DisplayAlert("sucesso", "Registro atualizado", "OK");
+            await Navigation.PopAsync();
+        }
+		catch (Exception ex)
+		{
+            await DisplayAlert("OPS", ex.Message, "OK");
+        }
+    }
+
 }
