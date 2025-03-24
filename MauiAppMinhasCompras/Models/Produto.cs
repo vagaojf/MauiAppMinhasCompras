c9ee40612a310e854bfd3d2a2c7fc1c67b796bc9
vagaojf <vagaojf@gmail.com>
2025-03-24 11:45:50 -0300

    validações

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,11 +4,45 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        String _descricao;
+        int _quantidade;
+        double _preco;
         [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public int Id { get;  set; }
+        public string Descricao {
+            get => _descricao;
+            set 
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor preencha a descrição");
+                }
+                _descricao = value;
+            } 
+        
+        }
+        public double Quantidade {
+            get => _quantidade;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor preencha a quantidade");
+                }
+                _quantidade = (int)value;
+            }
+        }
+        public double Preco {
+            get => _preco;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor preencha o preço");
+                }
+                _preco = (double)value;
+            }
+        }
         public double Total { get => Quantidade * Preco; }
     }
 
